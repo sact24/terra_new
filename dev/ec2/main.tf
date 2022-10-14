a5b805d44eca78173278b17f3a10d9212a1c93e2
sact24 <sact24@gmail.com>
2022-10-14 16:40:54 +0530

    Create main.tf

---
+++ dev/ec2/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+
+module "EC2" {
+  source = "../../../modules/EC2"
+
+//adding variables defined in root module
+  instance_name = "test"
+  key = "test"
+}
+
+
+output "IP" {
+  value = module.EC2
+}
